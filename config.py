3759c867bfcfdbc2dfd65cbdc1d3858edb1ff85e
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-02-11 23:05:06 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#reddit
+redditclid=("M-pFKo1ry2zb_v4XASOKBw")
+redditclscrt=("LDaeFpT9JJ6oR7s4voLX23jH6S-Haw")
+sub=("avatartrading")
+
+#discord
+bottoken=("MTA3NDA1ODAxMDA1NTQxNzg2Ng.GoBUhO.NyQZj-Q1gO3InueyiTw7dMcNaMvYPH20DE7sSw")
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+from config import bottoken
+from redditcount import subs
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!subs'):
+        await message.channel.send(subs)
+
+client.run(bottoken)
