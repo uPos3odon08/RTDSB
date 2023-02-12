c1077dd5b986be76d345691804ff8db460a2d4b0
uPos3odon08 <82838374+uPos3odon08@users.noreply.github.com>
2023-02-12 13:51:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,29 @@ import time
 from datetime import datetime
 from config import *
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!subs'):
-        await message.channel.send(subs)
-
-client.run(bottoken)
-
 while True:
+
     reddit = praw.Reddit(
         client_id=redditclid,
         client_secret=redditclscrt,
         user_agent="subreddit member count scraper 1.6 by u/Pos3odon08")
-
     now = datetime.now()
     subs = reddit.subreddit(sub).subscribers
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        if message.content.startswith('!subs'):
+            await message.channel.send(subs)
+
+    client.run(bottoken)
